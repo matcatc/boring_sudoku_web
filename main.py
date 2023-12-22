145649daa7272c6b178ebe98ee52155591e0e2d5
Matthew Todd <m_git@zerobitcoder.net>
2023-12-21 23:00:02 -0800

    Initial main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+Boring Sudoku Web
+So simple, it's almost boring.
+
+This is a simple web app that generates sudoku puzzles that can be played in
+the browser.
+
+@author Matthew Todd
+@date 2023-12-21
+'''
+
+def main():
+    print('main')
+
+if __name__ == '__main__':
+    main()
+
