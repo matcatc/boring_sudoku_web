3411e880785daa70a8450e8bf1a2dc3d35892f34
Matthew Todd <m_git@zerobitcoder.net>
2023-12-21 23:15:11 -0800

    Initial hello world cherrypy web app

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 '''
 Boring Sudoku Web
 So simple, it's almost boring.
@@ -10,8 +10,20 @@ the browser.
 @date 2023-12-21
 '''
 
+# We are using cherrypy since it's built-in server is robust enough for simple
+# production environments. This is compared to flask, bottle, and others that
+# say to gunincorn, uWSGI, or some other webapp, even cherrypy, which would be
+# another dependency. Also, cherrypy gives future flexibility to put a nginx
+# reverse proxy in front of it.
+import cherrypy
+
+class BoringSudokuWeb(object):
+    @cherrypy.expose
+    def index(self):
+        return "Hello world!"
+
 def main():
-    print('main')
+    cherrypy.quickstart(BoringSudokuWeb())
 
 if __name__ == '__main__':
     main()
